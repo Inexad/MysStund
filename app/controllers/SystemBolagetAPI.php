faa49520d47e70e705e634e7274b184831dfb992
Inexad <daniel.theliander@gmail.com>
2015-02-17 00:14:34 +0100

    Security Update

--- app/controllers/SystemBolagetAPI.php
+++ app/controllers/SystemBolagetAPI.php
@@ -1,43 +1,55 @@
 <?php
 
 class SystemBolagetAPI extends \BaseController {
-    /*
-    public function getLoadData(){
-        $drinks = simplexml_load_file('data/drinks.xml');      
-       
-        foreach ($drinks->artikel as $drink){
-            $newDrink = new Product();
-            $newDrink->articleId                = (String)$drink->Artikelid;
-            $newDrink->productId                = (String)$drink->Varnummer;
-            $newDrink->name                     = (String)$drink->Namn;
-            $newDrink->name2                    = (String)$drink->Namn2;
-            $newDrink->priceVAT                 = (String)$drink->Prisinklmoms;
-           // $newDrink->priceExVAT= $drink[0]->Prisexklmoms;
-            $newDrink->volume                   = (String)$drink->Volym;
-            $newDrink->priceperlitre            = (String)$drink->PrisPerLiter;
-            $newDrink->sellstart                = (String)$drink->Saljstart;
-            $newDrink->enddelivery              = (String)$drink->Slutlev;
-            $newDrink->productcategory          = (String)$drink->Varugrupp;
-            $newDrink->packaging                = (String)$drink->Forpackning;
-            $newDrink->sealing                  = (String)$drink->Forslutning;
-            $newDrink->origin                   = (String)$drink->Ursprung;
-            $newDrink->originlandname           = (String)$drink->Ursprunglandnamn;
-            $newDrink->producer                 = (String)$drink->Producent;
-            $newDrink->supplier                 = (String)$drink->Leverantor;
-            $newDrink->year                     = (String)$drink->Argang;
-            $newDrink->testedvintage            = (String)$drink->Provadargang;
-            $newDrink->alcoholic                = (String)$drink->Alkoholhalt;
-            $newDrink->range                    = (String)$drink->Sortiment;
-            $newDrink->organic                  = (String)$drink->Ekologisk;
-            $newDrink->kosher                   = (String)$drink->Koscher;
-            $newDrink->rawmaterialdescription   = (String)$drink->RavarorBeskrivning;
-            
-            $newDrink->save();
-        }
+    
+    /**
+     * Load all data.
+     *
+     */
+    public function getLoadData(){ 
+        //Most secure way to store an password.
+        $password   = "ollert";
+        $input      = Input::get("key");
         
-        return "Load completed";
+        if(isset($input)){
+            if(Input::get("key") == $password){
+                $drinks = simplexml_load_file('data/drinks.xml');  
+        
+                foreach ($drinks->artikel as $drink){
+                    $newDrink = new Product();
+                    $newDrink->articleId                = (String)$drink->Artikelid;
+                    $newDrink->productId                = (String)$drink->Varnummer;
+                    $newDrink->name                     = (String)$drink->Namn;
+                    $newDrink->name2                    = (String)$drink->Namn2;
+                    $newDrink->priceVAT                 = (String)$drink->Prisinklmoms;
+                   // $newDrink->priceExVAT= $drink[0]->Prisexklmoms;
+                    $newDrink->volume                   = (String)$drink->Volym;
+                    $newDrink->priceperlitre            = (String)$drink->PrisPerLiter;
+                    $newDrink->sellstart                = (String)$drink->Saljstart;
+                    $newDrink->enddelivery              = (String)$drink->Slutlev;
+                    $newDrink->productcategory          = (String)$drink->Varugrupp;
+                    $newDrink->packaging                = (String)$drink->Forpackning;
+                    $newDrink->sealing                  = (String)$drink->Forslutning;
+                    $newDrink->origin                   = (String)$drink->Ursprung;
+                    $newDrink->originlandname           = (String)$drink->Ursprunglandnamn;
+                    $newDrink->producer                 = (String)$drink->Producent;
+                    $newDrink->supplier                 = (String)$drink->Leverantor;
+                    $newDrink->year                     = (String)$drink->Argang;
+                    $newDrink->testedvintage            = (String)$drink->Provadargang;
+                    $newDrink->alcoholic                = (String)$drink->Alkoholhalt;
+                    $newDrink->range                    = (String)$drink->Sortiment;
+                    $newDrink->organic                  = (String)$drink->Ekologisk;
+                    $newDrink->kosher                   = (String)$drink->Koscher;
+                    $newDrink->rawmaterialdescription   = (String)$drink->RavarorBeskrivning;
+
+                    $newDrink->save();
+                    
+                    return "Load completed";
+                }  
+            }else return "Wrong key.";
+        }else return "Key needed to access this functionality";
     }
-    */
+    
     
     public function getShowAllDrinks(){
         $product = Product::all();
@@ -47,9 +59,9 @@ class SystemBolagetAPI extends \BaseController {
 
 
 	/**
-	 * Display the specified product.
+	 * Display the specified product by id.
 	 *
-	 * @return Response
+	 * @return result
 	 */
 	public function getById(){
         $product = Product::find(Input::get("id"));
@@ -58,9 +70,9 @@ class SystemBolagetAPI extends \BaseController {
 	}
     
     /**
-	 * Search for product
+	 * Search for product by search string.
 	 *
-	 * @return Response
+	 * @return result
 	 */
 	public function getSearch(){
         $result = Product::where("name2","LIKE", "%".Input::get("s")."%")
@@ -75,4 +87,15 @@ class SystemBolagetAPI extends \BaseController {
                         ->orWhere("producer",Input::get("s"))->get();
         return $result;  
 	}
+    
+    
+    /**
+	 * Search for product by alcoholic.
+	 *
+	 * @return result
+	 */
+	public function getByAlcoholic(){
+        $result = Product::where("alcoholic", "LIKE", "%".Input::get("alcoholic")."%")->get();
+        return $result;  
+	}    
 }
